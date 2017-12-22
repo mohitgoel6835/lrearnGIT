900f70431881ce36df0bbb2d7024e42df42493fd
mohitgoel6835 <mohitgoel6835@gmail.com>
2017-12-22 21:51:39 +0530

    adding hello,class

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
+package general;
 public class Hello{
 
 public static void main(String[] args){
 
 for(int i=1;i<=10;i++){
-System.out.println("Hello world"+i);
 System.out.println("Hello world"+i);
 }
 
